e320523a749cf1bb42f5037bad116060f5349931
Gyimesi Gábor <gyimgabor@gmail.com>
2014-11-25 17:08:59 +0100

    commit

--- opcache.php
+++ opcache.php
@@ -1,7 +1,7 @@
 <?php
 
 if(isset($_GET['config'])){
-ar_dump(opcache_get_configuration());
+var_dump(opcache_get_configuration());
 
 }elseif(isset($_GET['status'])){
 var_dump(opcache_get_status());
